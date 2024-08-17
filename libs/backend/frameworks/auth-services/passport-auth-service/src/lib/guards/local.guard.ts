e63c4f8cfbb9c83c9656a7278b4a64d3050f5494
0-BSCode <bryanipsanchez@gmail.com>
2024-08-17 21:07:07 +0800

    fix(passport-local-guard): create gql context

--- libs/backend/frameworks/auth-services/passport-auth-service/src/lib/guards/local.guard.ts
+++ libs/backend/frameworks/auth-services/passport-auth-service/src/lib/guards/local.guard.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class PassportLocalAuthGuard extends AuthGuard('local') {}
+export class PassportLocalAuthGuard extends AuthGuard('local') {
+  override getRequest(context: ExecutionContext) {
+    const request = GqlExecutionContext.create(context);
+    return request;
+  }
+}
